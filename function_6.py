a8e96748cacd1d15c7a84da1e4ef0aa81d4cc28f
smoolzone <smoolzone@gmail.com>
2020-09-14 21:12:54 +0300

    changed if condition from equal to bigger than changed the output

--- function_6.py
+++ function_6.py
@@ -6,15 +6,13 @@ def guess_num():
 
     user_guess = int(input("Guess A num:"))
 
-    if user_guess == random_num:
-        print("Well done u guess the no.", random_num)
+    if user_guess > random_num:
+        print("You win!!", random_num)
         return True
     else:
-        print("You failed and the right no is:", random_num)
+        print("You lost and the right no is:", random_num)
         return False
 
 
 while not guess_num():
     print("Try Again")
-
-print()
