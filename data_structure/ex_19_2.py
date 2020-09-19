1f00c87a8b299d4bfd73e2d37764ab114b4ac324
smoolzone <smoolzone@gmail.com>
2020-09-19 21:46:47 +0300

    above the mean with sys

--- data_structure/ex_19_2.py
+++ data_structure/ex_19_2.py
@@ -9,8 +9,8 @@ import sys
 from statistics import mean
 
 
-def get_above_average_grades(input_grads:list) -> list:
-    grades = [int(n) for n in input_grads]
+def get_above_average_grades(input_grades):
+    grades = [int(n) for n in input_grades]
     average = mean(grades)
     above_average = [n for n in grades if n > average]
     return above_average
